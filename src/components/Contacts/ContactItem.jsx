d77721f0410997f4f7cccd1064e3b289e9d7ea2d
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-03-14 16:56:49 +0200

    Add href for contact items

--- src/components/Contacts/ContactItem.jsx
+++ src/components/Contacts/ContactItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ContactItem = ({ title, text, icon }) => {
+const ContactItem = ({ title, text, linkHref, icon }) => {
   return (
     <li className="mb-3 flex items-center">
       <div className="mr-5 rounded-full bg-gunmetal p-2">{icon}</div>
@@ -8,7 +8,8 @@ const ContactItem = ({ title, text, icon }) => {
         <h5 className="title-h5">{title}</h5>
         <a
           className="font-dmsans text-xs font-medium text-gainsboro transition-all hover:text-floral md:text-sm"
-          href="#"
+          href={linkHref}
+          target="_blank"
         >
           {text}
         </a>
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -9,24 +9,28 @@ const contacts = [
   {
     title: "Email",
     text: "marinakovaleva259@gmail.com",
+    linkHref: "mailto:marinakovaleva259@gmail.com",
     icon: <Email className="h-5 w-5" />,
   },
 
   {
     title: "Website",
     text: "https://aldesign.it",
+    linkHref: "https://aldesign.it",
     icon: <LinkSimple className="h-5 w-5 fill-azureish-white" />,
   },
 
   {
     title: "Phone",
     text: "(063) 599 65 71",
+    linkHref: "tel:0635996571",
     icon: <Phone className="h-5 w-5" />,
   },
 
   {
     title: "Address",
     text: "Dnipro, Ukraine",
+    linkHref: "https://goo.gl/maps/JupyS6RAKmiAmLix7",
     icon: <Location className="h-5 w-5" />,
   },
 ];
