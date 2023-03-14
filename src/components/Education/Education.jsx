77e60518104e7e250691d7b2734af3522ea316ad
Marina Kovaleva <marinakovaleva259@gmail.com>
2023-03-14 14:02:57 +0200

    Changeinformation about courses for education component

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -5,17 +5,17 @@ import Circle from "../Icons/Circle";
 
 const education = [
   {
-    images: hillel,
-    organization: "Hillel IT school",
-    course: "Front-End Basic",
-    period: "November - January, 2022",
-  },
-
-  {
     images: coursera,
     organization: "Coursera",
     course: "Introduction to Git and GitHub",
     period: "Octobre, 2021",
+  },
+
+  {
+    images: hillel,
+    organization: "Hillel IT school",
+    course: "Front-End Basic, Front-End Pro",
+    period: "November - September, 2022",
   },
 
   {
